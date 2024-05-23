4b8e6f4ddc4b48603165bfa6b7f173b3a369b1a0
Joao Barroso <joao.barroso@alu.ufc.br>
2024-05-23 14:27:02 -0300

    add: h2 db for tests; refactor: should_be_able_to_create_a_new_job test

--- src/test/java/portfolio/joaom/gestaovagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/portfolio/joaom/gestaovagas/modules/company/controllers/CreateJobControllerTest.java
@@ -1,22 +1,20 @@
 package portfolio.joaom.gestaovagas.modules.company.controllers;
 
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import portfolio.joaom.gestaovagas.modules.company.dto.CreateJobDTO;
+import portfolio.joaom.gestaovagas.modules.company.entities.CompanyEntity;
+import portfolio.joaom.gestaovagas.modules.company.repositories.CompanyRepository;
 import portfolio.joaom.gestaovagas.utils.TestUtils;
-
-import java.util.UUID;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -24,12 +22,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateJobControllerTest {
 
     private MockMvc mvc;
 
     @Autowired
     private WebApplicationContext context;
+
+    @Autowired
+    private CompanyRepository companyRepository;
 
     @BeforeEach
     public void setup() {
@@ -41,6 +43,17 @@ public class CreateJobControllerTest {
 
     @Test
     public void should_be_able_to_create_a_new_job() throws Exception {
+        var company = CompanyEntity.builder()
+                .description("DESCRIPTION_TEST")
+                .email("EMAIL@TEST.COM")
+                .password("1234567890")
+                .name("NAME_TEST")
+                .username("USERNAME_TEST")
+                .build();
+
+        // save without having to wait for the transaction to commit
+        company = companyRepository.saveAndFlush(company);
+
         var createJobDTO = CreateJobDTO.builder()
                 .benefits("BENEFITS_TEST")
                 .description("DESCRIPTION_TEST")
@@ -51,7 +64,7 @@ public class CreateJobControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(TestUtils.objectToJSON(createJobDTO))
                         .header("Authorization", "Bearer " +
-                                TestUtils.generateToken(UUID.fromString("2ed64573-b174-4a2b-aca2-7cffe986ac04"))))
+                                TestUtils.generateToken(company.getId())))
                 .andExpect(status().isOk());
     }
 }
